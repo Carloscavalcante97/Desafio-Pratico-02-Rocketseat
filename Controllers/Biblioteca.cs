c2e33128e9070513433aa9c67c0d7ac69f93516d
Antônio Carlos Bezerra Cavalcante Junior <carlos.developer@icloud.com>
2024-10-11 13:55:48 -0300

    Add project files.

---
+++ Controllers/Biblioteca.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Desafio_Pratico_02_Rocketseat.Livros;
+
+namespace Desafio_Pratico_02_Rocketseat.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class Biblioteca : ControllerBase
+{
+    private static List<Livros> livros = new List<Livros>();
+    [HttpPost]
+    public IActionResult adicionarLivro([FromBody] Livros livro)
+    {
+        try
+        {
+            if (livros == null) throw new Exception("Livro inválido");
+            livros.Add(livro);
+            return Ok($"Livro: {livro.Titulo} foi adicionado com sucesso!");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    [HttpGet]
+    public IActionResult verLivros()
+    {
+        try
+        {
+            if (livros.Count == 0) throw new Exception("Lista vázia");
+            return Ok(livros);
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+    [Route("{id}")]
+    [HttpPut]
+    public IActionResult editarLivro(int id, [FromBody] LivroAtualizacaoDTO livroAtualizado)
+    {
+        try
+        {
+            var livro = livros.FirstOrDefault(I => I.Id == id);
+            if (livro == null) throw new Exception("Livro não encontrado");
+            livro.editarLivro(livroAtualizado);
+            return NoContent();
+
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+    [Route("{id}")]
+    [HttpDelete]
+    public IActionResult deletarLivro(int id)
+    {
+        try
+        {
+            var livro = livros.FirstOrDefault(I => I.Id == id);
+            if (livro == null) throw new Exception("Livro não encontrado");
+            livros.Remove(livro);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+
+    }
+}
---
+++ Livros.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio_Pratico_02_Rocketseat;
+
+public class Livros
+{
+
+    public static int contadorId = 1;
+    public int Id { get; private set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Genero { get; set; } = "Ficção";
+    public decimal Preco { get; set; }
+    public int QtdEstoque { get; set; }
+
+    public class LivroAtualizacaoDTO
+    {
+        public string? Titulo { get; set; }
+        public string? Autor { get; set; }
+        public string? Genero { get; set; }
+        public decimal? Preco { get; set; }
+        public int? QtdEstoque { get; set; }
+    }
+
+    public void editarLivro(LivroAtualizacaoDTO livroAtualizado)
+    {
+        if (!string.IsNullOrEmpty(livroAtualizado.Titulo))
+        {
+            Titulo = livroAtualizado.Titulo;
+        }
+
+        if (!string.IsNullOrEmpty(livroAtualizado.Autor))
+        {
+            Autor = livroAtualizado.Autor;
+        }
+
+        if (!string.IsNullOrEmpty(livroAtualizado.Genero))
+        {
+            Genero = livroAtualizado.Genero;
+        }
+
+        if (livroAtualizado.Preco.HasValue)
+        {
+            Preco = livroAtualizado.Preco.Value;
+        }
+
+        if (livroAtualizado.QtdEstoque.HasValue)
+        {
+            QtdEstoque = livroAtualizado.QtdEstoque.Value;
+        }
+    }
+    public Livros(string titulo, string autor, string genero, decimal preco, int qtdEstoque)
+    {
+        Id = contadorId++;
+        Titulo = titulo;
+        Autor = autor;
+        Genero = genero;
+        Preco = preco;
+        QtdEstoque = qtdEstoque;
+   
+    }
+
+}
